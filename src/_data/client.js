24d1aff68c9c44315955338f32b8517febc0a29c
Bezalel-Digital <theblessedkanu@gmail.com>
2025-02-27 10:30:03 -0400

    Add files via upload

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Happy Smile Dental Cllinic",
+    "email": "info@thehappysmiledentalclinic.com",
+    "phoneForTel": "846-7634",
+    "phoneFormatted": "(246) 846-7634",
+    "address": {
+        "lineOne": "123",
+        "lineTwo": "Tudor Street",
+        "city": "St. James",
+        "state": "Barbados",
+        "zip": "BB14009",
+        "country": "BB",
+        "mapLink": "https://maps.app.goo.gl/TPAP9WB8KZBabww77"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.example.com"
+};
